dbe8754af31f552fe9f23a4a58b39ce26db37f73
Lorenzo Lopes <lorenzo.lopes0808@gmail.com>
2025-09-15 10:24:21 -0300

    Update

--- exercicios/ex5.py
+++ exercicios/ex5.py
@@ -1,6 +1,7 @@
-alimento1 = "Alface"
-alimento2 = "Tomate"
-alimento3 = "Palmito"
+alimento1 = input("Digite o primeiro alimento: ")
+alimento2 = input("Digite o segundo alimento: ")
+alimento3 = input("Digite o terceiro alimento: ")
+
 
 receita = f"Hoje vou preparar uma salada de {alimento1}, {alimento2} e {alimento3}"
 
