4cd2d46a8a55632b2869c4ee53d03d7185b94355
Lorenzo Lopes <lorenzo.lopes0808@gmail.com>
2025-11-03 09:46:04 -0300

    LOOP - Lista 1: Ex01

--- LOOP/lista-01/ex01.py
+++ LOOP/lista-01/ex01.py
@@ -1,7 +1,8 @@
-n = 2
 n1 = float(input("Digite o primeiro valor: "))
 n2 = float(input("Digite o segundo valor: "))
-for i in range():
+for i in range(100):
     if(n2 == 0):
         n2 = float(input("Digite um segundo valor diferente de 0: "))
-        n = n + 1
+
+divisao = n1/n2
+print(divisao)
